bcdbec46129c7d848e78cb0c0c75d4878b1903c2
austindflatt <austindflatt@gmail.com>
2021-10-20 23:25:18 -0400

    created state for notes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
-import React from 'react'
+import React, { useState } from 'react'
 import Notes from './components/Notes';
 
 const App = () => {
+  const [notes, setNotes] = useState([{
+    id: Math.floor(Math.random() * 100000000),
+    noteTitle: "Note title",
+    noteText: "This is the note text",
+    noteDate: "Wed Oct 20 2021"
+  }])
   return (
     <div>
       <Notes />
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import Note from './Note'
-import { SimpleGrid} from '@mantine/core';
+import { SimpleGrid, Container } from '@mantine/core';
 
 const Notes = () => {
     return (
         <div>
+            <Container>
             <SimpleGrid cols={3}>
             <Note />
             <Note />
             <Note />
             </SimpleGrid>
+            </Container>
         </div>
     )
 }
