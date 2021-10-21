865086f1d8f040c6613b2a322324dae8776f48d3
austindflatt <austindflatt@gmail.com>
2021-10-20 21:41:26 -0400

    added mantine ui to Notes and Note components to give note list look

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import { Paper , Text, Space } from '@mantine/core';
 
 const Note = () => {
     return (
         <div>
-            <p>First note</p>
-            <div className="footer">
-                <p>Wed Oct 20th 2021</p>
-                <p>delete</p>
-            </div>
+            <Paper padding="md" shadow="sm" radius="md">
+                <Text>Note Title</Text>
+                <Text>
+                    Note Text
+                </Text>
+                <Text>
+                    Wed Oct 20th 2021 / delete
+                </Text>
+            </Paper>
+            <Space h="md" />
         </div>
     )
 }
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Note from './Note'
+import { SimpleGrid} from '@mantine/core';
 
 const Notes = () => {
     return (
         <div>
+            <SimpleGrid cols={3}>
             <Note />
+            <Note />
+            <Note />
+            </SimpleGrid>
         </div>
     )
 }
